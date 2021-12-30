b4d0c31c2ffa9926d08510c0bc5c323fec654bbd
Sami00002 <samuel.bodogan@emanuel.ro>
2021-12-30 13:34:29 +0200

    Add project files.

---
+++ Matrici 1/ArrayHelper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrici_1
+{
+    class ArrayHelper
+    {
+        public static bool Identitate(int[,] matrix)
+        {
+            bool esteIdentitate = true;
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+
+                    if (row != col)
+                    {
+                        if (matrix[row, col] != 0)
+                        {
+                            esteIdentitate = false;
+                        }
+                    }
+                    else if (matrix[row, col] != 1)
+                    {
+                        esteIdentitate = false;
+                    }
+                }
+            }
+            if (esteIdentitate == true)
+                return true;
+            else
+                return false;
+        }
+    }
+}
---
+++ Matrici 1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Matrici_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Dimensiunea matricei :");
+            int nr = Int32.Parse(Console.ReadLine());
+            int[,] matrix = new int[nr, nr];
+            int[,] matrix2 = new int[nr, nr];
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write($"matrix[{row},{col}]:");
+                    matrix[row, col] = Int32.Parse(Console.ReadLine());
+
+                }
+            }
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+
+                    Console.Write(matrix[row, col] + "\t");
+                    
+                    
+
+                }
+                Console.WriteLine();
+                        
+            }
+            if (ArrayHelper.Identitate(matrix) == true)
+                Console.WriteLine("Este matrice identitate!");
+            else
+                Console.WriteLine("Nu este matrice identitate!");
+        
+        }
+    }
+}
